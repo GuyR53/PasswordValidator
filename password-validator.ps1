3c47f55d9accb12cad602f02131cbf8408259d32
Guy Royburt <guyr@Guys-MacBook-Air.local>
2022-03-02 12:50:30 +0200

    finished powershell branch

--- password-validator.ps1
+++ password-validator.ps1
@@ -13,21 +13,21 @@ $Flag=0
 
 
 # Checking if length is minimum of 10 characters
-if ((${#Password} -lt 10))
+if (($password.length -lt 10))
 {
-    $OutMessage+="Password is less than 10 characters!'n";
+    $OutMessage+="Password is less than 10 characters!`n";
     $Flag=1
 }
 
 # Checking if pass contains both alphabet and number.
-if ($password -notmatch "^\d+$" -or $password -notmatch '\d')
+if ( (-not($password -match '\d'))-or ((-not ($password -cmatch '[A-Z]'))-and (-not ($password -cmatch '[a-z]'))))
 {
-    $OutMessage+="The Password must contain at least one letter and one digit!'n";
+    $OutMessage+="The Password must contain at least one letter and one digit!`n";
     $Flag=1
 }
 
 #Checking if pass include both small and capital case letters.
-if (($password -notcontains "[a-z]" -or $password -notcontains "[A-Z]"))
+if ((-not ($password -cmatch '[A-Z]'))-or (-not ($password -cmatch '[a-z]')))
 {
     $OutMessage+="The Password must contain at least one UPPERCASE and one LOWERCASE letter!";
     $Flag=1
@@ -37,12 +37,12 @@ if (($password -notcontains "[a-z]" -or $password -notcontains "[A-Z]"))
 if ( "$Flag" -eq 1 )
 {
 
-    echo ^<ESC^>[31m [31m$OutMessage[0m ;
+    echo  [31m$OutMessage[0m ;
     exit 1
 }
 else
 {
-    echo ^<ESC^>[32m [32m"Password is valid!"[0m
+    echo  [32m"Password is valid!"[0m
     exit 0
 }
 
